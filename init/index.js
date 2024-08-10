0f4c946678aeec61a5663f7bc92b2b5d8666a76d
Vinay Yadav <144893988+Vinayyy19@users.noreply.github.com>
2024-08-10 14:32:54 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -6,12 +6,12 @@ main().then(()=>{console.log("connected to database");})
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/WonderLust');
+  await mongoose.connect(process.env.dburl);
 }
 
 const data = async () =>{
     await listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj,owner:"668c1df6d158ea7384a28c90"}))
+    initData.data = initData.data.map((obj)=>({...obj,owner:process.env.demoUSer}))
     await listing.insertMany(initData.data);
     console.log("data inserted");
 }
